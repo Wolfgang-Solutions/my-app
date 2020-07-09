b311ccd826cdad6adc5e8b3a9cc17eb95c23e8eb
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-07-10 00:23:13 +0530

    Update App.vue
    fixed via GitHub

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,40 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos" />
+    <Header />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos';
-
+import Header from './components/layout/Header';
 export default {
-  name: 'App',
+  name:"app",
   components: {
-    Todos
-  },
-  data(){
-    return{
-       todos: [
-         {
-           id: 1,
-           title: "Todo 1",
-           completed: false
-         },
-         {
-           id: 2,
-           title: "Todo 2",
-           completed: true
-         },
-         {
-           id: 2,
-           title: "Todo 3",
-           completed: false
-         }
-       ]
-       }
+    Header
   }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
 </style>
