b5d6a59e48c7c611487f872f82e3ee35e3cbb783
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-07-09 20:54:43 +0530

    Simple (#2)
    * Create TodoItems.vue
    
    * Update TodoItems.vue
    
    * Update Todos.vue

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,17 @@
 <template>
 <div>
-    <div>
-</div>
+    <div v-bind:key="todo.id" v-for="todo in  todos"> </div>
+    <TodotItems v-bind:todo="todos"/>
 </div>    
 </template>
 
 <script>
+import TodoItems from './TodoItems.vue'
 export default {
     name: "Todos",
+    components: {
+        TodoItems
+    },
     props: ["todos"]
 }
 </script>
