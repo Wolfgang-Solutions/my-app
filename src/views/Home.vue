826e56765f86f54e26fee0e30e49988626fcac9d
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-07-10 14:51:10 +0530

    Update Home.vue
    changed paths

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todo v-bind:todos="todos"  v-on:del-todo="deleteTodo"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Header from '../components/layout/Header';
+import Todos from '../components/Todos';
+import AddTodo from '../components/AddTodo';
+import axios from 'axios';
 export default {
-  name: 'Home',
+  name:"Home",
   components: {
-    HelloWorld
+    Header,
+    Todos,
+    AddTodo
+  },
+  methods: {
+    deleteTodo(id) {
+      axios.delete(`http://jsonplaceholder.typicode/todos/${id}`)
+
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err => console.log(err))
+      
+      //this.todos = this.todos.filter(todo => todo.id!== id);
+    },
+    addTodo(newTodo) {     //whole parameter needs to be prased for new item on list
+      const {title, completed} = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed}) //PARAMETERS from constant
+      .then(res => this.todos = [...this.todos, res.data])
+      .catch(err => console.log(err));
+
+      //this.todos = [...this.todos, newTodo];  //SPREAD operator
+    },
+    created(){
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10') //will return a promise  [LIMIT SET AS 10]
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
+    }
   }
 }
 </script>
+
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+</style>
+
