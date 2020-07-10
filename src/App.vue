31801e7c2f6a9402605066b270dc60949bebafba
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-07-10 14:47:04 +0530

    Set Vue-Router

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,32 @@
 <template>
   <div id="app">
-    <Header />
-    <AddTodo v-on:add-todo="addTodo"/>
-    <Todo v-bind:todos="todos"  v-on:del-todo="deleteTodo"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
 
-<script>
-import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import axios from 'axios';
-export default {
-  name:"app",
-  components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  methods: {
-    deleteTodo(id) {
-      axios.delete(`http://jsonplaceholder.typicode/todos/${id}`)
-
-      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
-      .catch(err => console.log(err))
-      
-      //this.todos = this.todos.filter(todo => todo.id!== id);
-    },
-    addTodo(newTodo) {     //whole parameter needs to be prased for new item on list
-      const {title, completed} = newTodo;
-
-      axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed}) //PARAMETERS from constant
-      .then(res => this.todos = [...this.todos, res.data])
-      .catch(err => console.log(err));
-
-      //this.todos = [...this.todos, newTodo];  //SPREAD operator
-    },
-    created(){
-      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10') //will return a promise  [LIMIT SET AS 10]
-      .then(res => this.todos = res.data)
-      .catch(err => console.log(err));
-    }
-  }
-}
-</script>
-
-
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-  .btn:hover {
-    background: #666;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
